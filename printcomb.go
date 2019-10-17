fe6761242ff170d177cfea0e34b05288f4728a3f
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-17 12:33:01 +0600

    edited quest2-5

--- printcomb.go
+++ printcomb.go
@@ -6,13 +6,17 @@ func PrintComb(){
 	for i := '0'; i <= '9'; i++ {
 		for j := i + 1; j <= '9';j++ {
 			for k := j + 1; k <= '9'; k++ {
-				z01.PrintRune(i)
-				z01.PrintRune(j)
-				z01.PrintRune(k)
-			    if i != '7' || j != '8' || k != '9' {
+			    if i == '7' && j == '8' && k == '9' {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
+				}else{
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
-			    }
+				}
 		    }
 	    }
 	}  

0f38b39fc15d697e56346e2ba0a4f43e3234003f
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-17 12:42:06 +0600

    edited quest2-5

--- printcomb.go
+++ printcomb.go
@@ -4,13 +4,13 @@ import "github.com/01-edu/z01"
 
 func PrintComb(){
 	for i := '0'; i <= '9'; i++ {
-		for j := i + 1; j <= '9';j++ {
+		for j := i + 1; j <= '9'; j++ {
 			for k := j + 1; k <= '9'; k++ {
 			    if i == '7' && j == '8' && k == '9' {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
-				}else{
+				} else {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)

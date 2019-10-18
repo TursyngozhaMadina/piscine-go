d0c71f364266ebc064f22e28d931b416c010af25
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-18 21:24:33 +0600

    yuhfgl

---
+++ basicatoi.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func BasicAtoi(s string) int {
+	var x int
+	for _, y := range s {
+		z := 0
+		for i := '1'; i < 'y'; i ++ {
+			z++
+		}
+		x = x * 10 + z
+	}		
+
+return x
+}

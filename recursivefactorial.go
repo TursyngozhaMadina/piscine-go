1cf7798e1eac327e85e4ca207bc9791d57cc5230
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-21 14:14:05 +0600

    recursivtl

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -2,10 +2,10 @@ package piscine
 
 func RecursiveFactorial(nb int) int {
 
-	if nb == 1 && nb < 20 {
+	if nb == 1 {
 		return 1
 	}
-	if nb > 1 {
+	if nb > 1 && nb < 20 {
 		return nb * RecursiveFactorial(nb-1)
 	}
 	return 0

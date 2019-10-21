4d2bc1aea8940ea8e44afe89a57ddb92e6f8246a
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-21 14:26:34 +0600

    iterativepower

--- iterativepower.go
+++ iterativepower.go
@@ -1,13 +1,14 @@
 package piscine
+
 //Q43 Madina
 
 func IterativePower(nb int, power int) int {
 	if power < 0 {
-		return 0 
+		return 0
 	}
 	result := 1
-	for  i := 0 ; i < power ; i ++ {
+	for i := 0; i < power; i++ {
 		result = result * nb
-	} 
+	}
 	return result
 }

4fd4b2546c39ac8c35b0f0395e7295f01b01eeff
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-21 18:38:29 +0600

    findnextprime

--- findnextprime.go
+++ findnextprime.go
@@ -1,13 +1,10 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-
-	if nb > 1 {
-		if nb%2 == 0 {
-			return nb + 1
-		} else {
-			return nb
-		}
+	if nb%2 == 0 {
+		return nb + 1
+	} else {
+		return nb
 	}
 	return 0
 }

aaaefb3a99c164774f0ac4171970e803c5a9ff40
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-22 13:23:25 +0600

    alpha

--- alphacount.go
+++ alphacount.go
@@ -5,11 +5,10 @@ package piscine
 func AlphaCount(str string) int {
 	nb := 0
 	for _, let := range str {
-		if  ( let >= 'A' && let <= 'Z' ) || ( let >= 'a' && let <= 'z' ) {
-			nb ++	
+		if (let >= 'A' && let <= 'Z') || (let >= 'a' && let <= 'z') {
+			nb++
 		}
-				
+
 	}
-	return nb	
-} 
-	
+	return nb
+}

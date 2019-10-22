320ad1994a5947fdf99d3dc1a8d152680a05ff1b
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-23 00:36:10 +0600

    dtht

---
+++ toupper.go
@@ -0,0 +1,13 @@
+package piscine 
+
+func ToUpper(s string) string {
+	x := []rune(s)
+	result := " "
+	for i := 0; i <=len(x)-1; i++ {
+		if (x[i] >= 'a') && (x[i] <= 'z'){
+			x[i] = x[i] - 32
+		}
+		result += string(x[i])
+	}
+	return result
+}

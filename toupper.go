3493e731e4443bfeda20fee68174b9ed79015f7b
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-23 00:39:36 +0600

    dtht

--- toupper.go
+++ toupper.go
@@ -2,7 +2,7 @@ package piscine
 
 func ToUpper(s string) string {
 	x := []rune(s)
-	result := " "
+	result := ""
 	for i := 0; i <= len(x)-1; i++ {
 		if (x[i] >= 'a') && (x[i] <= 'z') {
 			x[i] = x[i] - 32

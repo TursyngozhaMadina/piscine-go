3d7c3c4669a5773e3924a3dfe0ad3c7e9cde090a
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-22 22:53:27 +0600

    gkxgkhjk'p

--- index.go
+++ index.go
@@ -2,23 +2,14 @@ package piscine
 
 func Index(s string, toFind string) int {
 	d := 0
-	k := 0
-	for i := range toFind {
-		if i == i {
-			k++
+	c := []rune(toFind)
+	for ind, book := range s {
+		if ind == ind {
+			d++
 		}
-	}
-	if k != 0 {
-		c := []rune(toFind)
-		for ind, book := range s {
-			if ind == ind {
-				d++
-			}
-			if c[0] == book && c[0] != 10 {
-				return ind
-			}
+		if c[0] == book && c[0] != 10 {
+			return ind
 		}
-		return -1
 	}
 	return 0
 }

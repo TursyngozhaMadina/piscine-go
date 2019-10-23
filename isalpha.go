5cc29b57c0160d2f76d1ca1074792ccd08e2892e
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-23 17:59:44 +0600

    isaago

--- isalpha.go
+++ isalpha.go
@@ -2,17 +2,23 @@ package piscine
 
 func IsAlpha(str string) bool {
 	x := []rune(str)
-	re := true
 	counter := 0
+	n := true
 	for range x {
 		counter++
 	}
 	for i := 0; i <= counter-1; i++ {
-		if re && (x[i] >= 'a' && x[i] <= 'z') || (x[i] >= '1' && x[i] <= '9') || (x[i] >= 'A' && x[i] <= 'Z') {
-			re = true
+		if n && (x[i] >= 33 && x[i] <= 47) {
+			n = false
+		} else if n && (x[i] >= 58 && x[i] <= 64) {
+			n = false
+		} else if x[i] >= 91 && x[i] <= 96 {
+			n = false
+		} else if x[i] >= 123 && x[i] <= 127 {
+			n = false
 		} else {
-			re = false
+			n = true
 		}
 	}
-	return re
+	return n
 }

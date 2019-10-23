6c767ce47ab485dc2de82aff867b6c2ca098ecc2
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-23 22:31:47 +0600

    drgjvkfth

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"github.com/01-edu/z01.PrintRune"
-	//"os"
+	"github.com/01-edu/z01"
+	"os"
 )
 
 func main() {
 
-	arguments := os.Args
+	result := os.Args
 
-	for _, i := range arguments[0] {
+	for _, i := range result[0] {
 
-		z01.PrintRune([i])
+		z01.PrintRune(i)
 	}
 	z01.PrintRune(10)
 }

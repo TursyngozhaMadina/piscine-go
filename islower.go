2d91de64fbe48c233542af9f0309ef864b82e5dd
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-23 19:11:45 +0600

    iscghtyfm

--- islower.go
+++ islower.go
@@ -2,10 +2,10 @@ package piscine
 
 func IsLower(str string) bool {
 	//x := []rune(str)
-	n := false
+	n := true
 	for _, cat := range str {
-		if cat >= 'A' && cat <= 'Z' || cat >= 'a' && cat <= 'z' {
-			n = true
+		if ( cat >= 33 && cat <= 47 || cat >= 58 && cat <= 64 || cat >= 91 && cat <= 96 || cat >= 123 && cat <= 126  ) {
+			n = false
 		}
 	}
 	return n

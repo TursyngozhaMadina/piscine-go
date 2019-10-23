30ad7c01f29b558c55e1f2d693e6ddd97735e031
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-23 18:41:09 +0600

    isao2

--- isalpha.go
+++ isalpha.go
@@ -4,7 +4,7 @@ func IsAlpha(str string) bool {
 	//x := []rune(str)
 	n := true
 	for _, cat := range str {
-		if !( cat >= '0' && cat <= '9' || cat >= 'A' && cat <= 'Z' || cat >= 'a' && cat <= 'z') {
+		if !(cat >= '0' && cat <= '9' || cat >= 'A' && cat <= 'Z' || cat >= 'a' && cat <= 'z') {
 			n = false
 		}
 	}

2894d3d1d711feed251d1e3e81592181641859b8
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-23 19:14:32 +0600

    iscghtyfm

--- islower.go
+++ islower.go
@@ -4,7 +4,7 @@ func IsLower(str string) bool {
 	//x := []rune(str)
 	n := true
 	for _, cat := range str {
-		if ( cat >= 33 && cat <= 47 || cat >= 58 && cat <= 64 || cat >= 91 && cat <= 96 || cat >= 123 && cat <= 126  ) {
+		if cat >= 33 && cat <= 47 || cat >= 58 && cat <= 64 || cat >= 91 && cat <= 96 || cat >= 123 && cat <= 126 {
 			n = false
 		}
 	}

39739ce8750f7a923b735d8c49b8c284ca10d91f
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-23 15:52:37 +0600

    iklurg

--- capitalize.go
+++ capitalize.go
@@ -3,7 +3,11 @@ package piscine
 func Capitalize(s string) string {
 	x := []rune(s)
 	resul := ""
-	for i := 0; i <= len(x)-1; i++ {
+	counter := 0
+	for range x {
+		counter++
+	}
+	for i := 0; i <= counter-1; i++ {
 		if ((x[i] == ' ') || (x[i] == '+')) && (x[i+1] >= 'a') && (x[i+1] <= 'z') {
 			x[i+1] = x[i+1] - 32
 		}

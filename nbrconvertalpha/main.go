9a05d2e70b5c8ec305007a091b262bbf82ba7948
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-24 22:57:51 +0600

    jf,hdgo

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -30,10 +30,10 @@ func main() {
 	pos := 1
 	flagupper := false
 	c := 0
-	//for range arg {
-	//	c++
-	//}
-	if arg[2] == "--upper" {
+	for range arg {
+		c++
+	}
+	if c >= 2 && arg[1] == "--upper" {
 		pos = 1
 		flagupper = true
 	}
@@ -43,11 +43,11 @@ func main() {
 			if num == -1 {
 				z01.PrintRune(' ')
 			} else {
-				if flagupper {
-					z01.PrintRune(rune('a' + num -32))
+				if !flagupper  {
+					z01.PrintRune(rune('a' + num - 1))
 					//z01.PrintRune(rune('a' + num -32))
 				} else {
-					z01.PrintRune(rune('A' + num - 32))
+					z01.PrintRune(rune('A' + num - 1))
 					//z01.PrintRune(rune('A' + num - 32))
 				}
 			}

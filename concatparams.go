a8c0ad441dbedfd94f59ce4655dda1443c9f42ff
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-25 00:03:50 +0600

    m,jlkdieotfyhr

---
+++ concatparams.go
@@ -0,0 +1,19 @@
+package piscine
+
+func ConcatParams(args []string) string {
+
+	var a string
+	var count int = 0
+	
+	for range args {
+		count++
+	}
+	for let := range args {
+		a += args[let]
+		
+		if let != count - 1 {
+			a += "\n"
+		}
+	}
+	return a
+}

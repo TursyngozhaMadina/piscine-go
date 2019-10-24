58b02f7581156e30a1ea9435adff554b6e54a029
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-24 13:21:06 +0600

    sortparamh

--- sortparams/main.go
+++ sortparams/main.go
@@ -8,9 +8,9 @@ import (
 func main() {
 	result := os.Args
 	//count := 0
-	for j := '!'; j <= '~' ; j++ {
+	for j := '!'; j <= '~'; j++ {
 		for i := range result {
-            if i != 0  {
+			if i != 0 {
 				for _, i2 := range result[i] {
 					if i2 == j {
 						z01.PrintRune(i2)
@@ -18,6 +18,6 @@ func main() {
 					}
 				}
 			}
-		}		  
+		}
 	}
 }
